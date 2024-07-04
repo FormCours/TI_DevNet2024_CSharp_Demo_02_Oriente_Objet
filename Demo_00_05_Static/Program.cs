1a0e1f3dc9279ef5099aaca88d7dc405a2da454c
Aurélien <aurelien.strimelle@bstorm.be>
2024-07-04 16:13:57 +0200

    Demo 05 - Static 🐫

---
+++ Demo_00_05_Static/Program.cs
@@ -0,0 +1,18 @@
+﻿using Demo_OO_05_Static.Tools;
+
+using static System.Console;
+using static System.Math;
+//using static Demo_OO_05_Static.Tools.ExempleTool;
+
+
+Console.WriteLine("Veuillez encoder deux nombre");
+Write("> ");
+double nb1 = int.Parse(ReadLine()!);
+
+Write("> ");
+double nb2 = int.Parse(ReadLine()!);
+
+double res = Round(nb1 / nb2);
+WriteLine($"Resultat : {res}");
+
+ExempleTool.Valeur = 50;
---
+++ Demo_00_05_Static/Tools/ExempleTool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OO_05_Static.Tools
+{
+    public static class ExempleTool
+    {
+        public static int Valeur { get; set; }
+
+        public static double Round(double val)
+        {
+            return Convert.ToInt32(val);
+        }
+    }
+}
