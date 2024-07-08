482000863e7cc4b75b21da24fff17782dad8e3ff
Aurélien <aurelien.strimelle@bstorm.be>
2024-07-08 12:16:25 +0200

    Demo 09 - Delegate 🌷

---
+++ Demo_OO_09_Delegue/Program.cs
@@ -0,0 +1,26 @@
+﻿using Demo_OO_09_Delegue.Utils;
+
+List<int> numbers = [5, 32, 42, 1,  -5, 21, 99, -52, -5, 10, 2, 100];
+
+Console.WriteLine("Trie : Ordre croissant");
+Console.WriteLine("**********************");
+bool TestCroissant(int val1, int val2)
+{
+    return val1 > val2;
+}
+List<int> numbersTrie1 = CollectionUtils.Trier(numbers, TestCroissant);
+Console.WriteLine($"Original : {string.Join(" > ", numbers)}");
+Console.WriteLine($"Trié     : {string.Join(" > ", numbersTrie1)}");
+
+Console.WriteLine("Trie : Ordre décroissant");
+Console.WriteLine("************************");
+//TrierDelegate del = delegate (int val1, int val2) { return val1 < val2; };
+List<int> numbersTrie2 = CollectionUtils.Trier(numbers, delegate (int val1, int val2) { return val1 < val2; });
+Console.WriteLine($"Original : {string.Join(" > ", numbers)}");
+Console.WriteLine($"Trié     : {string.Join(" > ", numbersTrie2)}");
+
+Console.WriteLine("Trie : Alphabetique");
+Console.WriteLine("************************");
+List<int> numbersTrie3 = CollectionUtils.Trier(numbers, (val1, val2) => string.Compare(val1.ToString(), val2.ToString()) > 0);
+Console.WriteLine($"Original : {string.Join(" > ", numbers)}");
+Console.WriteLine($"Trié     : {string.Join(" > ", numbersTrie3)}");
---
+++ Demo_OO_09_Delegue/Utils/CollectionUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OO_09_Delegue.Utils
+{
+    public delegate bool TrierDelegate(int nb1, int nb2);
+    public static class CollectionUtils
+    {
+        public static List<int> Trier(List<int> numbers, TrierDelegate compare)
+        {
+            // - Copie de la liste
+            List<int> result = new List<int>(numbers);
+            //List<int> result = new(numbers);
+            //List<int> result = [..numbers];
+
+            // Algo -> Trie a bulles
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int k = 0; k < (result.Count -i - 1); k++)
+                {
+                    if (compare(result[k], result[k + 1]))
+                    {
+                        int temp = result[k];
+                        result[k] = result[k + 1];
+                        result[k + 1] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
